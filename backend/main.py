fd5443c55796d6ffab00df4a30e1facca0ee6156
izat <izat@a.com>
2025-10-30 17:21:12 +0800

    added options to corsorigins methods

--- backend/main.py
+++ backend/main.py
@@ -62,7 +62,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["Authorization", "Content-Type"],
 )
 logging.info(f"CORS origins set to: {origins if origins else 'None'}")
